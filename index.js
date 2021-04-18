56d544aac26719bae8a9421f8fb367ffff248b74
nischal08 <nischal.karanjit@gmail.com>
2021-04-18 06:27:05 +0545

    news gone

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 <html>
-<body>
+  <body>
     <module-one>
-        <ul>
-            <li>item 1</li>
-            <li>item 4</li>
-            <li>item 5</li>
-        </ul>
+      <ul>
+        <li>item 1</li>
+        <li>item 4</li>
+        <li>item 5</li>
+      </ul>
     </module-one>
-    <module-two>
-
-
-
-<div>This is worker div added</div>
-
-    </module-two>
-
-    <module-three>
-        <div>my awesome new module about news</div>
-
-    </module-three>
-</body>
-
-</html>
+  </body>
+</html>;
